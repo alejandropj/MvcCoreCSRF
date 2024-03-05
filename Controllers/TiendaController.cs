f741ae0ff02743e193e9b882a196ad5de56767df
alejandropj <alejandroparrajimenez@gmail.com>
2024-03-05 10:50:32 +0100

    Add project files.

---
+++ Controllers/TiendaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreCSRF.Controllers
+{
+    public class TiendaController : Controller
+    {
+        public IActionResult Productos()
+        {
+            if(HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("AccesoDenegado", "Managed");
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Productos (string direccion, string[] producto)
+        {
+            if (HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("AccesoDenegado", "Managed");
+            }
+            else
+            {
+                //Para enviar informacion entre controladores se utiliza 
+                //TempData["KEY"]
+                TempData["DIRECCION"] = direccion;
+                TempData["PRODUCTOS"] = producto;
+                return RedirectToAction("PedidoFinal");
+            }
+        }
+        public IActionResult PedidoFinal()
+        {
+            string[] productos = TempData["PRODUCTOS"] as string[];
+            ViewData["DIRECCION"] = TempData["DIRECCION"].ToString();
+            return View(productos);
+        }
+    }
+}
